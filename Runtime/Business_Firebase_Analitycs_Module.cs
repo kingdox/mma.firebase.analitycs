a18ae64e1f618ebee2111bed3fd4ebea7e656123
Kingdox <arpaxavier@gmail.com>
2022-06-19 19:03:49 +0200

    Add files via upload

---
+++ Runtime/Business_Firebase_Analitycs_Module.cs
@@ -0,0 +1,56 @@
+#region Access
+//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Threading.Tasks;
+//using UnityEngine;
+using Firebase.Analytics;
+#endregion
+namespace MMA.Business_Firebase_Analitycs
+{
+    public static partial class Key
+    {
+        // public const string _   = KeyData._;
+        public static string LogEvent = "Business_Firebase_Analitycs_LogEvent";
+    }
+    public static partial class Import
+    {
+        //public const string _ = _;
+    }
+    public sealed partial class Business_Firebase_Analitycs_Module : Module
+    {
+        #region References
+        //[Header("Applications")]
+        //[SerializeField] public ApplicationBase interface_Business_Firebase_Analitycs;
+        #endregion
+        #region Reactions ( On___ )
+        // Contenedor de toda las reacciones del Business_Firebase_Analitycs
+        protected override void OnSubscription(bool condition)
+        {
+            //LogEvent
+            Middleware<string>.Subscribe_Publish(condition,name,LogEvent);
+            Middleware<(string name, Parameter parameter)>.Subscribe_Publish(condition,name,LogEvent);
+            Middleware<(string name, Parameter[] parameter)>.Subscribe_Publish(condition,name,LogEvent);
+        }
+        #endregion
+        #region Methods
+        // Contenedor de toda la logica del Business_Firebase_Analitycs
+        private void LogEvent(string name){
+            FirebaseAnalytics.LogEvent(name);
+        }
+        private void LogEvent((string name, Parameter parameter) result){
+            FirebaseAnalytics.LogEvent(result.name, result.parameter);
+        }
+        private void LogEvent((string name, Parameter[] parameter) result)
+        {
+            FirebaseAnalytics.LogEvent(result.name, result.parameter);
+        }
+        #endregion
+        #region Request ( Coroutines )
+        // Contenedor de toda la Esperas de corutinas del Business_Firebase_Analitycs
+        #endregion
+        #region Task ( async )
+        // Contenedor de toda la Esperas asincronas del Business_Firebase_Analitycs
+        #endregion
+    }
+}
